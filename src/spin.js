412c6a19ded32c42cdbd64d8e465fb14b0bb4505
chemzqm <chemzqm@gmail.com>
2016-05-23 22:56:33 +0800

    improve torgb function

--- src/spin.js
+++ src/spin.js
@@ -12,8 +12,12 @@ function createCtx(node) {
   return ctx
 }
 
+const hex_reg = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i
 function torgb(hex) {
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  if (hex.length == 4) hex = hex.replace(/[^#]/g, function (p) {
+    return p + p
+  })
+  let result = hex_reg.exec(hex)
   return result ? {
       r: parseInt(result[1], 16),
       g: parseInt(result[2], 16),
