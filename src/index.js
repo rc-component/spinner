6959502248c96082143b58383a6fec4b866c7d71
chemzqm <chemzqm@gmail.com>
2017-07-22 16:20:07 +0800

    Release 1.0.0

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import assign from 'object-assign'
 import spin from './spin'
 import ReactDom from 'react-dom'
